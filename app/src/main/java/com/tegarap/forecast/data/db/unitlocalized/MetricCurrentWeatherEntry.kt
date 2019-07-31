f17dfadea1130ae6f0bc3dcb246be12666d9df2f
tegarap <tegara946@gmail.com>
2019-07-31 13:55:23 +0700

    entity

--- app/src/main/java/com/tegarap/forecast/data/db/unitlocalized/MetricCurrentWeatherEntry.kt
+++ app/src/main/java/com/tegarap/forecast/data/db/unitlocalized/MetricCurrentWeatherEntry.kt
@@ -3,21 +3,21 @@ package com.tegarap.forecast.data.db.unitlocalized
 import androidx.room.ColumnInfo
 
 class MetricCurrentWeatherEntry(
-    @ColumnInfo(name = "tempF")
+    @ColumnInfo(name = "tempC")
     override val temperatures: Double,
     @ColumnInfo(name = "condition_text")
     override val conditionText: String,
     @ColumnInfo(name = "condition_icon")
     override val conditionIconUrl: String,
-    @ColumnInfo(name = "windMph")
+    @ColumnInfo(name = "windKph")
     override val windSpeed: Double,
     @ColumnInfo(name = "windDir")
     override val windDirection: String,
-    @ColumnInfo(name = "precipIn")
+    @ColumnInfo(name = "precipMm")
     override val precipitationVolume: Double,
-    @ColumnInfo(name = "feelslikeF")
+    @ColumnInfo(name = "feelslikeC")
     override val feelsLikeTemperature: Double,
-    @ColumnInfo(name = "visMiles")
+    @ColumnInfo(name = "visKm")
     override val visibilityDistance: Double
 
 ) : UnitSpecificCurrentWeatherEntry
